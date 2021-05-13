c36487e2b5e04827dfbd822253239a1ae188748e
priyanka1432 <77844823+priyanka1432@users.noreply.github.com>
2021-05-13 12:53:35 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-#To print the greatest number
- 
-x=int(input("Enter the first number"))
-y=int(input("Enter the first number"))
-z=int(input("Enter the first number"))
+# This is a sample Python script.
 
-
-
-if x>=y and x>=z:
-    print(x)
-elif y>=x and y>=z:
-    print(y)
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+x=int(input("Enter the number"))
+if x<0:
+    print("It's a negative number")
 else:
-    print(z)
+    print("It's a positive number")
