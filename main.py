6e7de2d0a4ac58f1ff8183331a907ad2e1a87aa8
priyanka1432 <77844823+priyanka1432@users.noreply.github.com>
2021-05-13 12:49:25 +0530

    commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+x=int(input("Enter the number"))
+if x<0:
+    print("It's a negative number")
+else:
+    print("It's a positive number")
