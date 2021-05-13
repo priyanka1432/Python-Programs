525949367b81286fdd0bfdf8b2158c7c09f893ca
priyanka1432 <77844823+priyanka1432@users.noreply.github.com>
2021-05-13 13:33:45 +0530

    commit

--- program-3*/main.py
+++ program-3*/main.py
@@ -1,1 +1,13 @@
+# This is a sample Python script.
 
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+i=1
+while i<=100:
+    if i%3==0 or i%5==0:
+        i+=1
+    else:
+       print(i)
+       i+=1
