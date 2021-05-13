6f47816a0e6e7ef2c09b7e8940d758fb103732c9
priyanka1432 <77844823+priyanka1432@users.noreply.github.com>
2021-05-13 13:27:47 +0530

    Add files via upload

---
+++ Program-2*/main.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+x=int(input("Enter the first number"))
+y=int(input("Enter the first number"))
+z=int(input("Enter the first number"))
+
+
+
+if x>=y and x>=z:
+    print(x)
+elif y>=x and y>=z:
+    print(y)
+else:
+    print(z)
