a68dc173f531b9ef63b1b5c577935909cb32891f
priyanka1432 <77844823+priyanka1432@users.noreply.github.com>
2021-05-14 13:45:43 +0530

    Add files via upload

---
+++ program5/prime number or not.py
@@ -0,0 +1,12 @@
+# To check the given number is positive or not
+x=int(input("Enter the number"))
+if x<=1:
+    print("not prime")
+else:
+    for i in range(2,int(x/2)):
+        if x%i==0:
+            print("not prime number")
+            break
+    else:
+       print("prime number")
+
